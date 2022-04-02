237b3bc94056ae28b9ad956bec818a024572a116
alsalmiw <alsalmiw@gmail.com>
2022-04-02 15:18:22 -0700

    pages and components

---
+++ src/Components/FooterComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FooterComponent() {
+  return (
+    <div>FooterComponent</div>
+  )
+}
---
+++ src/Pages/ServicesPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ServicesPage() {
+  return (
+    <div>ServicesPage</div>
+  )
+}
