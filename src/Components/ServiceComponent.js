3e2c638862371983a516d6eadfddc3f579590cd3
alsalmiw <alsalmiw@gmail.com>
2022-04-02 18:21:36 -0700

    colored icons

--- src/Components/ServiceComponent.js
+++ src/Components/ServiceComponent.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './service.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faGavel, faBriefcase, faBabyCarriage, faLandmark, faCar, faSchool, faPiggyBank } from '@fortawesome/free-solid-svg-icons';
+import {faGavel, faBriefcase, faBabyCarriage, faLandmark, faCar, faSchool, faPiggyBank, faBurger, faBriefcaseMedical, faFileSignature, faBuildingShield, faPersonMilitaryPointing, faSignHanging, faPassport, faWallet, faLightbulb, faEnvelopesBulk } from '@fortawesome/free-solid-svg-icons';
 
-export default function ServiceComponent() {
+export default function ServiceComponent(props) {
+   const [serviceIcon, setServiceIcon] = useState('')
+   const [btnClass, setBtnClass]=useState('')
+    useEffect(()=>{
+     setServiceIcon(props.service.icon) 
+     btnStyle()
+      },[])
+
+      
+    console.log(props)
+    let {service, idx} = props
+    let index=0;
+    const btnStyle = () => {
+        let colors = ["service-btn blue-purple", 'service-btn pink-red', "service-btn yellow-lime", "service-btn green-blue" , "service-btn orange-yellow" ]
+        index = idx%colors.length
+        setBtnClass(colors[index]) 
+    
+}
+   
+
   return (
-    <div>
-        <div className='service-btn blue-purple'>
+    <div key={idx} className='service-container'>
+        <div className={btnClass}>
         <FontAwesomeIcon icon={faGavel}/>
         </div>
-        <div><p>service title</p></div>
+        <div><p>{service.serviceName}</p></div>
     </div>
   )
 }
--- src/Services/DataService.js
+++ src/Services/DataService.js
@@ -2,6 +2,7 @@
 async function GetAllServices(){
     let res = await fetch('http://localhost:5098/services/getallservices');
     let data = await res.json();
+    console.log(data)
     return data;
 }
 
