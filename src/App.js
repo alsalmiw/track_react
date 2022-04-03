5380ea5a1bd1b79a8f2445b08b80699a8ae8826f
sophieespejo <sophmeas@gmail.com>
2022-04-02 17:18:51 -0700

    added navbar and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useContext} from 'react'
 import ServicesContext from './Context/ServicesContext';
 import UseServices from './Hooks/use-services';
 import ServicesPage from './Pages/ServicesPage';
+import NavbarComponent from './Components/NavbarComponent';
+import FooterComponent from './Components/FooterComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -9,8 +11,10 @@ function App() {
   return (
    <ServicesContext.Provider value={UseServices()}>
    <div>
+     <NavbarComponent/>
      <p>Hello</p>
      <ServicesPage />
+     <FooterComponent />
    </div>
   </ServicesContext.Provider>
 
--- src/Components/NavbarComponent.js
+++ src/Components/NavbarComponent.js
@@ -1,37 +1,31 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
-import logo from '../Images/logo'
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import logo from '../Images/Logo.svg'
+import "./Components.css"
+
 
 export default function NavbarComponent() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand >
-            <img src={logo}/>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Services</Nav.Link>
-              <Nav.Link href="#link">Sponsor</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <Row className="justify-content-center">
+      <Col xs={12} md={9}>
+        <Navbar bg="" expand="lg">
+          <Container fluid>
+            <Navbar.Brand >
+              <img src={logo}/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+              <Nav className="">
+                <Nav.Link href="#home" className="navLinkTxt ">Services</Nav.Link>
+                <Nav.Link href="#link" className="">Sponsors</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Col>
+    </Row>
     </>
   );
 }
