28d16422f7ba24bfe3119befa052246a97ce58bb
sophieespejo <sophmeas@gmail.com>
2022-04-03 10:14:27 -0700

    added cols to sponsors page

--- src/Pages/SponsorsPage.js
+++ src/Pages/SponsorsPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Row, Col, Container} from 'react-bootstrap'
 import CalaverasCommunityLogo from '../Images/Calaveras Community Foundation Logo@3x.png'
 import DignityHealthLogo from '../Images/Dignity Health Logo@3x.png'
-import PhinellsLogo from '../Images/Pinnells Logo@2x.png'
+import PhinellsLogo from '../Images/Pinnells Logo@3x.png'
 import TacoBellLogo from '../Images/Taco Bell Logo@3x.png'
 import SierraPacificLogo from '../Images/Sierra Pacific Logo@3x.png'
 import CalaverasLogo from '../Images/Calaveras Logo@3x.png'
@@ -13,13 +13,25 @@ export default function SponsorsPage() {
       <Col xs={12} md={9}>
         <p className="m-0"><strong>Sponsors</strong></p>
         <p className="">Thank you to our sponsors!</p>
-        <Row className="mt-2">
-          <img className="calComLogo" src={CalaverasCommunityLogo}/>
-          <img className="dignityLogo" src={DignityHealthLogo}/>
-          <img className="phinellsLogo" src={PhinellsLogo}/>
-          <img className="tacoBellLogo" src={TacoBellLogo}/>
-          <img className="sierraLogo" src={SierraPacificLogo}/>
-          <img className="calaverasLogo" src={CalaverasLogo}/>
+        <Row className="mt-2 sponsorRow">
+          <Col>
+            <img className="sponsorImg" src={CalaverasCommunityLogo}/>
+          </Col>
+          <Col>
+            <img className="sponsorImg dignityLogo" src={DignityHealthLogo}/>
+          </Col>
+          <Col>
+            <img className="sponsorImg phinellsLogo" src={PhinellsLogo}/>
+          </Col>
+          <Col>
+            <img className="sponsorImg tacoBellImg" src={TacoBellLogo}/>
+          </Col>
+          <Col>
+            <img className="sponsorImg" src={SierraPacificLogo}/>
+          </Col>
+          <Col>
+            <img className="sponsorImg calaverasLogo" src={CalaverasLogo}/>
+          </Col>
         </Row>
       </Col>
 
