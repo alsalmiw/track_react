7ebb2d0b9c10ac7d7a08533775cee27721160c9e
sophieespejo <sophmeas@gmail.com>
2022-04-17 16:07:06 -0700

    edited footer

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -8,7 +8,7 @@ export default function FooterComponent() {
     <Row className="footerBg pt-4 pb-3 justify-content-center">
       <Col xs={12} md={10} lg={9}>
         <Row className="d-flex mx-lg-2 mx-5">
-          <Col xs={10} md={7} lg={5}className="d-flex">
+          <Col xs={12} md={7} lg={5}className="d-flex">
             <img className="calLogo" alt="Calaveras Logo" src={CalaverasLogo} />
             <p className="footerTxt pt-1">On the right track is a product of the Calaveras Chamber of Commerce. All rights reserved.</p>
           </Col>
